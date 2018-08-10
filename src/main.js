bc4a067a73028276371737646caabbc865293c9d
Eric Shull <eshulldesign@gmail.com>
2018-08-10 12:11:15 -0700

    add two different forms and submit functions for symptoms and doctors

--- src/main.js
+++ src/main.js
@@ -7,15 +7,13 @@ import { DoctorService } from './doctor_service';
 
 $(document).ready(function() {
 
-  $('#searchTerm').submit(function(event) {
+  $('#symptomForm').submit(function(event) {
     event.preventDefault();
     console.log("form submit");
-    let input = $("#searchTerm").find('input[name=symptoms]').val();
+    let symptomInput = $("#symptomForm").find('input[name=symptoms]').val();
     $('#searchTerm').val("");
     let doctorService = new DoctorService();
-    let symptomPromise = doctorService.getSymptomByInput(input);
-    let doctorPromise = doctorService.getSymptomByInput(input);
-
+    let symptomPromise = doctorService.getSymptomByInput(symptomInput);
 
       symptomPromise.then(function(response) {
         console.log("response " + response );
@@ -29,16 +27,29 @@ $(document).ready(function() {
          $('#doctorsSymptoms').append("<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>");
          }
       });
+
+  });
+
+  $('#doctorForm').submit(function(event) {
+    event.preventDefault();
+    console.log("form submit");
+    let doctorInput = $("#doctorForm").find('input[name=doctorsInfo]').val();
+    $('#searchTerm').val("");
+    let doctorService = new DoctorService();
+    let doctorPromise = doctorService.getSymptomByInput(doctorInput);
+
       doctorPromise.then(function(response) {
         console.log("response " + response );
        let body = JSON.parse(response);
        console.log(JSON.parse(response));
        let array = body.data;
+       // irst name, last name, address, phone number, website and whether or not the doctor is accepting new patients
        for (var i = 0; i < array.length; i++) {
          console.log("here");
-         $('#doctorsSymptoms').append("<h2>" + array[i].profile.first_name + " " + array[i].profile.last_name + " " +  array[i].profile.title + "</h2>");
-         $('#doctorsSymptoms').append("<h5>" + array[i].specialties[0].description + "</h5>");
-         $('#doctorsSymptoms').append("<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>");
+         $('#doctorsInformation').append("<h2>" + array[i].practices[0].visit_address + " " + array[i].practices[0].office_hours + " " + array[i].practices[0].phones + "</h2>");
+         $('#doctorsInformation').append("<h3>" + array[i].profile.first_name + " " + array[i].profile.last_name + " " +  array[i].profile.title + "</h3>");
+         $('#doctorsInformation').append("<h5>" + array[i].specialties[0].description + "</h5>");
+         $('#doctorsInformation').append("<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>");
          }
       });
 

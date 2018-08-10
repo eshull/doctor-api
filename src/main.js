f743d26a0ba3e1fdb5bc479ba68e6ea092079941
Eric Shull <eshulldesign@gmail.com>
2018-08-10 15:21:31 -0700

    add styling and layout for forms on index.html

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
     $('#searchTerm').val("");
     let doctorService = new DoctorService();
     let symptomPromise = doctorService.getDoctorBySymptom(symptomInput);
-    console.log(symptomPromise);
 
     symptomPromise.then(function(response) {
      let body = JSON.parse(response);
@@ -31,7 +30,6 @@ $(document).ready(function() {
        } else {
            let array = body.data;
            for (var i = 0; i < array.length; i++) {
-             console.log("here");
              $('#doctorsSymptoms').append("<h2>" + array[i].profile.first_name + " " + array[i].profile.last_name + " " +  array[i].profile.title + "</h2>");
              $('#doctorsSymptoms').append("<h5>" + array[i].specialties[0].description + "</h5>");
              $('#doctorsSymptoms').append("<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>");
@@ -56,20 +54,20 @@ $(document).ready(function() {
        console.log(JSON.parse(response));
        let array = body.data;
        for (var i = 0; i < array.length; i++) {
-         console.log("here");
-         $('#doctorsInformation').append("<h1>" +
+
+         $('#doctorsInformation').append("<div class='doctor'>" + "<h1>" +
          array[i].profile.first_name + " " +
          array[i].profile.last_name + " " +
-         array[i].profile.title + "</h1>");
-         $('#doctorsInformation').append("<h4>" + array[i].specialties[0].description + "</h4>");
-         $('#doctorsInformation').append("<h2>" + array[i].practices[0].visit_address.street + " " + array[i].practices[0].visit_address.city + " " +
+         array[i].profile.title + "</h1>" +
+         "<h4>" + array[i].specialties[0].description + "</h4>" +
+         "<h2>" + array[i].practices[0].visit_address.street + " " + array[i].practices[0].visit_address.city + " " +
          array[i].practices[0].visit_address.state + " " +
-         array[i].practices[0].visit_address.zip + " " + "</h2>");
-         $('#doctorsInformation').append("<h2>" + "Phone: " + array[i].practices[0].phones[0].number + "</h2>");
-         $('#doctorsInformation').append("<h2>" + "Accepting new patients? " + array[i].practices[0].accepts_new_patients + "</h2>");
-         $('#doctorsInformation').append("<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>");
+         array[i].practices[0].visit_address.zip + " " + "</h2>" +
+         "<h2>" + "Phone: " + array[i].practices[0].phones[0].number + "</h2>" +
+         "<h2>" + "Accepting new patients? " + array[i].practices[0].accepts_new_patients + "</h2>" +
+         "<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>" +
+        "</div>");
          }
       });
-
   });
 });

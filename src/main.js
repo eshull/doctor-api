1f0aede3fe521314297640f80114f798b3a1efaa
Eric Shull <eshulldesign@gmail.com>
2018-08-10 16:16:58 -0700

    refactor main.js doctor by name search to include message on return of no results

--- src/main.js
+++ src/main.js
@@ -6,18 +6,8 @@ import './styles.css';
 import { DoctorService } from './doctor_service';
 
 $(document).ready(function() {
-
   $('#symptomForm').submit(function(event) {
     event.preventDefault();
-    // if (document.getElementById("#doctorsSymptoms") == null){
-    //   return " "
-    // } else {
-    //   (function clear(){
-    //     return document.getElementById("#doctorsSymptoms").innerHTML = ""
-    //   }());
-    // }
-
-    console.log("form submit");
     let symptomInput = $("#symptomForm").find('input[name=symptoms]').val();
     $('#searchTerm').val("");
     let doctorService = new DoctorService();
@@ -44,32 +34,32 @@ $(document).ready(function() {
 
   $('#doctorForm').submit(function(event) {
     event.preventDefault();
-    console.log("form submit");
     let doctorInput = $("#doctorForm").find('input[name=doctorsInfo]').val();
     $('#searchTerm').val("");
     let doctorService = new DoctorService();
     let doctorPromise = doctorService.getDoctorByName(doctorInput);
 
       doctorPromise.then(function(response) {
-        console.log("response " + response );
        let body = JSON.parse(response);
-       console.log(JSON.parse(response));
-       let array = body.data;
-       for (var i = 0; i < array.length; i++) {
-
-         $('#doctorsInformation').append("<div class='doctor'>" + "<h1>" +
-         array[i].profile.first_name + " " +
-         array[i].profile.last_name + " " +
-         array[i].profile.title + "</h1>" +
-         "<h4>" + array[i].specialties[0].description + "</h4>" +
-         "<h2>" + array[i].practices[0].visit_address.street + " " + array[i].practices[0].visit_address.city + " " +
-         array[i].practices[0].visit_address.state + " " +
-         array[i].practices[0].visit_address.zip + " " + "</h2>" +
-         "<h2>" + "Phone: " + array[i].practices[0].phones[0].number + "</h2>" +
-         "<h2>" + "Accepting new patients? " + array[i].practices[0].accepts_new_patients + "</h2>" +
-         "<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>" +
-        "</div>");
-         }
+       if (body.data.length == 0) {
+           $('#doctorsSymptoms').append("<h3>" + "Search returned no results" + "</h3>");
+       } else {
+           let array = body.data;
+           for (var i = 0; i < array.length; i++) {
+             $('#doctorsInformation').append("<div class='doctor'>" + "<h1>" +
+             array[i].profile.first_name + " " +
+             array[i].profile.last_name + " " +
+             array[i].profile.title + "</h1>" +
+             "<h4>" + array[i].specialties[0].description + "</h4>" +
+             "<h2>" + array[i].practices[0].visit_address.street + " " + array[i].practices[0].visit_address.city + " " +
+             array[i].practices[0].visit_address.state + " " +
+             array[i].practices[0].visit_address.zip + " " + "</h2>" +
+             "<h2>" + "Phone: " + array[i].practices[0].phones[0].number + "</h2>" +
+             "<h2>" + "Accepting new patients? " + array[i].practices[0].accepts_new_patients + "</h2>" +
+             "<p>" + "<h3>" + "Bio:" + "</h3>" + array[i].profile.bio + "</p>" +
+            "</div>");
+             }
+        }
       });
   });
 });
